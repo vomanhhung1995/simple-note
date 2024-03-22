e53a68a575b577ba0c0a251260d268045bfc32e6
vomanhhung1995 <vomanhhung1995@gmail.com>
2024-03-22 21:07:42 +0700

    Change origin: config.url

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -31,7 +31,7 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
     preflightContinue: true,
-    origin: '*',
+    origin: config.url,
   }));
 
   app.use('/api/v0/', IndexRouter);
